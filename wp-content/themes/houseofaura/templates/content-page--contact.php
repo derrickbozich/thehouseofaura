6333db7b557e9e98d0220593d3e634950e61feba
Derrick <dbozich1@gmail.com>
2019-01-07 00:17:00 -0700

    fixed a few things

--- wp-content/themes/houseofaura/templates/content-page--contact.php
+++ wp-content/themes/houseofaura/templates/content-page--contact.php
@@ -1,18 +1,22 @@
 <?php the_content(); ?>
-  <div class="info">
-    <div class="icons">
-      <a href="https://soundcloud.com/user-320086851" target="_blank">
-        <i class="fab fa-soundcloud fa-2x"></i>
-      </a>
 
-      <a href="https://soundcloud.com/user-320086851" target="_blank">
-         <i class="fab fa-instagram fa-2x"></i>
-      </a>
-    </div>
-    <p>
-      <a href="mailto:thehouseofauramusic@gmail.com">thehouseofauramusic@gmail.com</a>
-    </p>  
+    <div class="info">
+      <div class="icons">
+        <a href="https://soundcloud.com/user-320086851" target="_blank">
+          <i class="fab fa-soundcloud fa-2x"></i>
+        </a>
+
+        <a href="https://soundcloud.com/user-320086851" target="_blank">
+           <i class="fab fa-instagram fa-2x"></i>
+        </a>
+      </div>
+      <p>
+        <a href="mailto:thehouseofauramusic@gmail.com">thehouseofauramusic@gmail.com</a>
+      </p>
+  
+
+
+  <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+
+
   </div>
-
-
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
--- wp-content/themes/houseofaura/templates/footer.php
+++ wp-content/themes/houseofaura/templates/footer.php
@@ -1,45 +1,52 @@
-<footer class="content-info">
-  <div id="sketch-holder">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-    <script type="text/javascript">
-      let t = 0.1;
-      let numCircles = 8;
+<footer class="footer">
+  <div class="container">
+    <div id="sketch-holder">
+      <?php dynamic_sidebar('sidebar-footer'); ?>
+      <script type="text/javascript">
+        let t = 0.1;
+        let numCircles = 8;
 
-      function setup() {
-        var width = windowWidth * 0.07;
-        var canvas = createCanvas(width, width);
-        canvas.parent('sketch-holder');
-        noStroke();
-      }
+        function setup() {
+          var width = windowWidth * 0.05;
+          var canvas = createCanvas(width, width);
+          canvas.parent('sketch-holder');
+          noStroke();
+        }
 
-      function draw() {
-       translate(width/2, height/2);
-       circles(width/2, width * 0.06, 0, radians(t));
-       t += 0.15;
-      }
+        function draw() {
+         translate(width/2, height/2);
+         circles(width/2, width * 0.06, 0, radians(t));
+         t += 0.15;
+        }
 
-      function circles(radius, rSub,angle, aAdd) {
-        push();
-          for (let i = numCircles; i > 0; i--){
-            if(i % 2 == 0){
-              c = 0;
-            } else {
-              c = 255;
+        function circles(radius, rSub,angle, aAdd) {
+          push();
+            for (let i = numCircles; i > 0; i--){
+              if(i % 2 == 0){
+                c = 0;
+              } else {
+                c = 255;
+              }
+              fill(c);
+              ellipse(0, 0, radius * 2, radius * 2);
+              radius -= rSub;
+              angle += aAdd;
+              let r = rSub * 0.7;
+              let x = cos(angle + aAdd) * r;
+              let y = sin(angle + aAdd) * r;
+              translate(x, y);
             }
-            fill(c);
-            ellipse(0, 0, radius * 2, radius * 2);
-            radius -= rSub;
-            angle += aAdd;
-            let r = rSub * 0.7;
-            let x = cos(angle + aAdd) * r;
-            let y = sin(angle + aAdd) * r;
-            translate(x, y);
-          }
-        pop();
-      }
-    </script>
+          pop();
+        }
+      </script>
 
+    </div>
+    <div class="footer-logo">
+      The House of Aura
+    </div>
+    <div class="footer-info">
+      <div class="copyright">&copy; 2019</div>
+      <a href="http://www.derrickbozich.com"><div class="author">by derrick bozich</div></a>
+    </div>
   </div>
-  <div class="copyright">&copy; 2019</div>
-  <a href="http://www.derrickbozich.com"><div class="author">by derrick bozich</div></a>
 </footer>
