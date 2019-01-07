bb8beb9b67b50a2a4b3f1b8b4118b395ae1974c5
Derrick <dbozich1@gmail.com>
2019-01-07 00:32:54 -0700

    fixed a few things

--- wp-content/themes/houseofaura/page-contact.php
+++ wp-content/themes/houseofaura/page-contact.php
@@ -1,9 +1,4 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php //get_template_part('templates/page', 'header'); ?>
-  <div class="contact">
-    <div class="contact-card">
-      <?php get_template_part('templates/content', 'page--contact'); ?>
-    </div>
-  </div>
-
+    <?php get_template_part('templates/content', 'page--contact'); ?>
 <?php endwhile; ?>
--- wp-content/themes/houseofaura/templates/content-page--contact.php
+++ wp-content/themes/houseofaura/templates/content-page--contact.php
@@ -1,22 +1,19 @@
 <?php the_content(); ?>
+<div class="contact">
+  <div class="info">
+    <div class="icons">
+      <a href="https://soundcloud.com/user-320086851" target="_blank">
+        <i class="fab fa-soundcloud fa-2x"></i>
+      </a>
 
-    <div class="info">
-      <div class="icons">
-        <a href="https://soundcloud.com/user-320086851" target="_blank">
-          <i class="fab fa-soundcloud fa-2x"></i>
-        </a>
+      <a href="https://soundcloud.com/user-320086851" target="_blank">
+         <i class="fab fa-instagram fa-2x"></i>
+      </a>
+    </div>
+    <p>
+      <a href="mailto:thehouseofauramusic@gmail.com">thehouseofauramusic@gmail.com</a>
+    </p>
 
-        <a href="https://soundcloud.com/user-320086851" target="_blank">
-           <i class="fab fa-instagram fa-2x"></i>
-        </a>
-      </div>
-      <p>
-        <a href="mailto:thehouseofauramusic@gmail.com">thehouseofauramusic@gmail.com</a>
-      </p>
-  
-
-
-  <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-
-
+    <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
   </div>
+</div>
