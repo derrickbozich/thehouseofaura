8bc5e567f132aae9b0033a45f5cec294e1f48767
Derrick <dbozich1@gmail.com>
2019-04-06 02:38:59 -0600

    image link fixes

--- wp-content/themes/houseofaura/templates/content-page--home.php
+++ wp-content/themes/houseofaura/templates/content-page--home.php
@@ -1,5 +1,5 @@
 <?php // the_content(); ?>
 <div class="container home" id="home">
-  <img src="http://devlocal.the-house-of-aura.com/wp-content/uploads/2019/01/fairytalesofchar00perr-1.png" alt="">
+  <img src="<?=  get_template_directory_uri(); ?>/dist/images/home.png">
   <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
 </div>
